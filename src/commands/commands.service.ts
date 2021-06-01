86b4f36deaa651ef60aa271c1e4318986a7fea65
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2021-06-02 00:22:14 +0200

    put data on queue

--- src/commands/commands.service.ts
+++ src/commands/commands.service.ts
@@ -3,6 +3,7 @@ import got from 'got';
 import { RunDto } from './dto/run.dto';
 import { GoRestResponse } from './interfaces/go-rest-response';
 import { transformPersonalData } from './common/transform';
+import {connect} from 'amqplib'
 
 @Injectable()
 export class CommandsService {
@@ -11,6 +12,10 @@ export class CommandsService {
   async run(runDto: RunDto): Promise<void> {
     const body = await got(`https://gorest.co.in/public-api/users/${runDto.id}`).json() as GoRestResponse;
     const transformedData = transformPersonalData(body.data)
+    const connection = await connect(process.env.AMQP)
+    const channel = await connection.createChannel()
+    channel.sendToQueue(process.env.QUEUE, Buffer.from(JSON.stringify(transformedData)))
+
     this.logger.log(JSON.stringify(transformedData))
   }
 }
