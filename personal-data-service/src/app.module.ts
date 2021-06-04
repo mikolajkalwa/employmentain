722d29dd6e825d3d91c9236d10e43f7adfde4d83
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2021-06-04 13:04:25 +0200

    Add logging interceptor

--- personal-data-service/src/app.module.ts
+++ personal-data-service/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { CommandsModule } from './commands/commands.module';
+import { LoggingInterceptor } from './common/logging.interceptor';
 
 @Module({
   imports: [CommandsModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ personal-data-service/src/common/logging.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Logger,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const req = context.switchToHttp().getRequest();
+    return next
+      .handle()
+      .pipe(
+        tap(() =>
+          Logger.log(`${req.method} ${req.url}`, context.getClass().name),
+        ),
+      );
+  }
+}
