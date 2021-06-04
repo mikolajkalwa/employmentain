2e5c2e57e891d20a0429e603a018bafcbb5c564d
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2021-06-04 13:48:10 +0200

    handle command service errors

--- personal-data-service/src/commands/commands.service.ts
+++ personal-data-service/src/commands/commands.service.ts
@@ -5,6 +5,7 @@ import { transformPersonalData } from '../common/transform';
 import { HttpClient } from '../common/httpClient';
 import { MessageBroker } from '../common/messageBroker';
 import { PersonalData } from './interfaces/personal-data';
+import got from 'got/dist/source';
 
 @Injectable()
 export class CommandsService {
@@ -16,28 +17,39 @@ export class CommandsService {
   ) {}
 
   async run(runDto: RunDto): Promise<void> {
-    const personalDataServiceResponse = await this.httpClient.getPersonalData(
-      runDto.id,
-    );
-
-    if (personalDataServiceResponse.code !== 200) {
-      this.logger.warn(
-        `Third party service returned non 200 response code.
-        Response: ${JSON.stringify(personalDataServiceResponse)}`,
+    try {
+      const personalDataServiceResponse = await this.httpClient.getPersonalData(
+        runDto.id,
       );
-      return;
+
+      if (personalDataServiceResponse.code !== 200) {
+        this.logger.warn(
+          `Third party service returned non 200 response code.
+          Response: ${JSON.stringify(personalDataServiceResponse)}`,
+        );
+        return;
+      }
+
+      const transformedData = transformPersonalData(
+        personalDataServiceResponse.data as PersonalData,
+      );
+
+      this.logger.log(
+        `Transformed ${JSON.stringify(
+          personalDataServiceResponse.data,
+        )} into ${JSON.stringify(transformedData)}`,
+      );
+
+      await this.messageBroker.putOnQueue(JSON.stringify(transformedData));
+    } catch (err) {
+      if (err instanceof got.RequestError) {
+        this.logger.error(
+          `Access to 3rd party service failed: ${err.message}`,
+          err.stack,
+        );
+      } else {
+        this.logger.error('Unknown error occured', JSON.stringify(err));
+      }
     }
-
-    const transformedData = transformPersonalData(
-      personalDataServiceResponse.data as PersonalData,
-    );
-
-    this.logger.log(
-      `Transformed ${JSON.stringify(
-        personalDataServiceResponse.data,
-      )} into ${JSON.stringify(transformedData)}`,
-    );
-
-    await this.messageBroker.putOnQueue(JSON.stringify(transformedData));
   }
 }
